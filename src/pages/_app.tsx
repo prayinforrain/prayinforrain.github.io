82b3e4c2f6138617f352c3c365def2e404e64a04
prayinforrain <dldnwo99@naver.com>
2025-02-05 15:38:48 +0900

    chore: init chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "dayjs/locale/ko";
 import dayjs from "dayjs";
+import { Provider } from "@/components/ui/provider";
 
 dayjs.locale("ko");
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko" suppressHydrationWarning>
+      <Head>
+        <link
+          rel="preload"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+          as="style"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
