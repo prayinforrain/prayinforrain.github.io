0ed5ee7346ded876d99da8b55d07948ab9df68e9
prayinforrain <dldnwo99@naver.com>
2025-02-05 17:12:40 +0900

    refactor: remove unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import { Button } from "@chakra-ui/react";
 import DefaultLayout from "@/components/layout/DefaultLayout";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
