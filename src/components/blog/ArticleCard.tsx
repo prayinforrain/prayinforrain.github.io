1c0ed9e5416a648eed93bdf17e3634d0761d0015
prayinforrain <dldnwo99@naver.com>
2025-02-26 17:05:57 +0900

    feat: 아티클카드에 썸네일 표시

--- src/components/blog/ArticleCard.tsx
+++ src/components/blog/ArticleCard.tsx
@@ -1,15 +1,18 @@
 import { Post } from "@/util/post";
 import {
+  Box,
   CardBody,
   CardFooter,
   CardHeader,
   CardRoot,
   CardRootProps,
   Heading,
+  Image,
   LinkOverlay,
   Text,
 } from "@chakra-ui/react";
 import dayjs from "dayjs";
+import NextImage from "next/image";
 import Link from "next/link";
 
 interface ArticleCardProps extends CardRootProps {
@@ -22,29 +25,61 @@ const ArticleCard = ({ post, ...props }: ArticleCardProps) => {
       key={post.slug}
       as="article"
       _hover={{ bg: "bg.subtle" }}
+      flexDirection="row"
+      overflow="hidden"
       {...props}
     >
-      <CardHeader>
-        <LinkOverlay asChild>
-          <Link href={post.slug}>
-            <Heading size="md" lineClamp={1}>
-              {post.title}
-            </Heading>
-          </Link>
-        </LinkOverlay>
-      </CardHeader>
-      <CardBody>
-        <Text fontSize="sm" lineClamp={2}>
-          {post.description || post.contentPreview}
-        </Text>
-      </CardBody>
-      <CardFooter>
-        <Text fontSize="sm" color="gray.500" textAlign="right" w="full" asChild>
-          <time dateTime={post.createdAt}>
-            {dayjs(post.createdAt).format("YYYY.MM.DD")}
-          </time>
-        </Text>
-      </CardFooter>
+      {post.thumbnail && (
+        <Image
+          rounded="md"
+          fit="cover"
+          asChild
+          marginX="auto"
+          h="full"
+          alignSelf="center"
+          maskImage="linear-gradient(to right, rgba(0,0,0,0.4), rgba(0,0,0,0))"
+          maskSize="80%"
+          maskRepeat="no-repeat"
+          maskPosition="left"
+          position="absolute"
+        >
+          <NextImage
+            src={post.thumbnail}
+            alt={post.title}
+            width={200}
+            height={100}
+          />
+        </Image>
+      )}
+      <Box w="full">
+        <CardHeader>
+          <LinkOverlay asChild>
+            <Link href={post.slug}>
+              <Heading size="md" lineClamp={1}>
+                {post.title}
+              </Heading>
+            </Link>
+          </LinkOverlay>
+        </CardHeader>
+        <CardBody>
+          <Text fontSize="sm" lineClamp={2}>
+            {post.description || post.contentPreview}
+          </Text>
+        </CardBody>
+        <CardFooter>
+          <Text
+            fontSize="sm"
+            color="gray.500"
+            textAlign="right"
+            w="full"
+            asChild
+          >
+            <time dateTime={post.createdAt}>
+              {dayjs(post.createdAt).format("YYYY.MM.DD")}
+            </time>
+          </Text>
+        </CardFooter>
+      </Box>
     </CardRoot>
   );
 };
