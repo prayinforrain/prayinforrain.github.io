e6544ab9e2dddef66d67a82bf23353f37aeb2fe3
prayinforrain <dldnwo99@naver.com>
2025-02-26 14:02:41 +0900

    feat: 깃허브 링크

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,13 +1,20 @@
 import { Flex, IconButton, Text } from "@chakra-ui/react";
+import Link from "next/link";
 import { LuGithub } from "react-icons/lu";
 
 const Footer = () => {
   return (
     <Flex direction="column" gap={2} alignItems="center" mt={8} as="footer">
       <Flex as="address" direction="row" gap={2}>
-        <IconButton variant="ghost">
-          <LuGithub />
-        </IconButton>
+        <Link
+          href="https://github.com/prayinforrain"
+          legacyBehavior
+          target="_blank"
+        >
+          <IconButton variant="ghost" as="a">
+            <LuGithub />
+          </IconButton>
+        </Link>
       </Flex>
       <Text textStyle="xs" color="fg.subtle">
         © 2025 PrayinforRain. All rights reserved.
