7444fe088e61ccae260d038ba1ec20ad3ebf5d02
prayinforrain <dldnwo99@naver.com>
2025-03-17 12:46:54 +0900

    feat: integrate Google and Naver analytics with environment variables

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import "dayjs/locale/ko";
 import dayjs from "dayjs";
 import { Provider } from "@/components/ui/provider";
 import { Toaster } from "@/components/ui/toaster";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
+import ENV from "@/util/env";
 
 dayjs.locale("ko");
 
@@ -11,6 +13,30 @@ export default function App({ Component, pageProps }: AppProps) {
     <Provider>
       <Component {...pageProps} />
       <Toaster />
+      {/* Analytics */}
+      {process.env.NODE_ENV === "production" ? (
+        <>
+          <GoogleAnalytics gaId={ENV.GOOGLE_ANALYTICS_ID} />
+          <GoogleTagManager gtmId={ENV.GOOGLE_TAG_MANAGER_ID} />
+          {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+          <script
+            type="text/javascript"
+            src="//wcs.naver.net/wcslog.js"
+          ></script>
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+          if(!wcs_add) var wcs_add = {};
+          wcs_add["wa"] = "${ENV.NAVER_ANALYTICS_ID}";
+          if(window.wcs) {
+            wcs_do();
+          }
+          `,
+            }}
+          />
+        </>
+      ) : null}
     </Provider>
   );
 }
