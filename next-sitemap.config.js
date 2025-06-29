660521c8ec97784fd3f2d720e554ec878f1ba90f
PrayinForRain <46566891+prayinforrain@users.noreply.github.com>
2025-06-29 12:56:40 +0900

    chore: sitemap 형식 변경 감지를 위해 사이트맵 파일명 변경 -- 구글 검색 콘솔에서 sitemap.xml 파일의 유형이 변경되었음을 인지하지 못함(사이트맵 -> 사이트맵 색인)

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,6 +4,7 @@ module.exports = {
   siteUrl:
     process.env.NEXT_PUBLIC_BASE_URL || "https://prayinforrain.github.io",
   generateRobotsTxt: true,
+  sitemapBaseFileName: "sitemaps",
   robotsTxtOptions: {
     additionalSitemaps: [
       `${process.env.NEXT_PUBLIC_BASE_URL}/archived-blog/sitemap.xml`,
